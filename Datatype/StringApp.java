50bfe38224e1712c149b0c97a9ea9b42dd853f73
안효인 <kjr04205@nate.com>
2021-12-16 21:59:08 +0900

    datatype update

---
+++ Datatype/StringApp.java
@@ -0,0 +1,18 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		// Character VS String
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character
+		System.out.println("H"); // String
+		
+		System.out.println("Hello " + "World"); // String
+		
+		System.out.println("Hello \nWorld"); // new line
+		
+		System.out.println("Hello \"World\""); // escape // Hello "World"
+
+	}
+
+}
