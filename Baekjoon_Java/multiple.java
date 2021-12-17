8df698151ee5512a5fee9d6883bcac192b197d5f
안효인 <kjr04205@nate.com>
2021-12-17 23:06:27 +0900

    Variable

---
+++ Baekjoon_Java/multiple.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class multiple {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a,b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a * (b % 10));
+		System.out.println(a * ((b % 100) / 10)); // (472 * ((385 % 100) / 10))
+		System.out.println(a * (b / 100));
+		System.out.println(a * b);
+	}
+}
+
+
---
+++ Datatype/Casting.java
@@ -0,0 +1,16 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		double a = 1.1;
+		double b = 1;
+		System.out.println(a);
+		System.out.println(b);
+		
+		String c = Integer.toString(1);
+		System.out.println(c.getClass());
+		
+		System.out.println(((385 % 100) / 10));
+	}
+
+}
