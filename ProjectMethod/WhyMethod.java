1331e485efa47016734b7d5ae123a6b076c320d6
안효인 <kjr04205@nate.com>
2021-12-31 23:52:25 +0900

    method pratice

--- ProjectMethod/WhyMethod.java
+++ ProjectMethod/WhyMethod.java
@@ -1,14 +1,32 @@
 
 public class WhyMethod {
 	
-	public static void printTwoTimesA() {
-		System.out.println("-");
-		System.out.println("A");
-		System.out.println("A");
-	}
 	public static void main(String[] args) {
-		printTwoTimesA();
-
+		printTwoTimes("a");
+		printTwoTimes("b");
+		printTwoTimes("1");
+		
+		printTwoTimesA("a", "-");
+		printTwoTimesA("b", "*");
+		printTwoTimesA("c", "/");
 	}
-
+	public static void printTwoTimes(String text) {
+		System.out.println("-");
+		System.out.println(text);
+		System.out.println(text);
+	}
+	public static void printTwoTimesA(String text, String delimiter) {
+		System.out.println(text);
+		System.out.println(delimiter);
+	}
+	/*public static void printTwoTimesA() {
+	System.out.println("-");
+	System.out.println("A");
+	System.out.println("A");
+	}
+	public static void printTwoTimesB() {
+		System.out.println("-");
+		System.out.println("B");
+		System.out.println("B");
+	}*/
 }
