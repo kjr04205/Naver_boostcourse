91fb13dc372161199c760e5cea645e775d186973
안효인 <kjr04205@nate.com>
2021-12-31 23:44:34 +0900

    method

---
+++ ProjectMethod/WhyMethod.java
@@ -0,0 +1,14 @@
+
+public class WhyMethod {
+	
+	public static void printTwoTimesA() {
+		System.out.println("-");
+		System.out.println("A");
+		System.out.println("A");
+	}
+	public static void main(String[] args) {
+		printTwoTimesA();
+
+	}
+
+}
